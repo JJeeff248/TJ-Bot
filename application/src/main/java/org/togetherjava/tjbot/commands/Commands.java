c20bb1862b7f9a498a346c376b381a763c274081
java-coding-prodigy <java-coding-prodigy@github.com>
2021-11-11 14:29:07 +0530

    POJOs fix

--- application/src/main/java/org/togetherjava/tjbot/commands/Commands.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/Commands.java
@@ -43,8 +43,7 @@ public enum Commands {
         // available.
         return List.of(new PingCommand(), new DatabaseCommand(database), new TeXCommand(),
                 new TagCommand(tagSystem), new TagManageCommand(tagSystem),
-                new TagsCommand(tagSystem), new VcActivityCommand(),
-                new WolframAlphaCommand());
+                new TagsCommand(tagSystem), new VcActivityCommand(), new WolframAlphaCommand());
 
     }
 }
